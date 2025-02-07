ffc0d65689b2f0fdb428b29cd355e0a8e4abd449
Julien Hwang <jhwang@lcs.on.ca>
2025-02-07 11:59:16 -0500

    Initial Commit

---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by Julien Hwang on 7/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
