c2cdf28653d7116d5c4977023641a9de9a1fc9f7
Julien Hwang <jhwang@lcs.on.ca>
2025-02-07 12:32:43 -0500

    Added animations

---
+++ LottieAnimationsList/Model/FavouriteAnimation.swift
@@ -0,0 +1,22 @@
+//
+//  FavouriteAnimation.swift
+//  LottieAnimationsList
+//
+//  Created by Julien Hwang on 7/2/2025.
+//
+
+import Foundation
+
+struct FavouriteAnimation: Identifiable {
+    
+    let id = UUID()
+    let filename: String
+    let description: String
+    
+}
+
+let favourites = [
+    FavouriteAnimation(filename: Animation - 1738948844100, description: "Gaming Controller")
+    ,
+    FavouriteAnimation(filename: Animation - 1738949187570, description: "Loading")
+]
